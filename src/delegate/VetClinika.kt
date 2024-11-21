29b84d8cf4514ec9c46ffa80d8d986efc0e802d4
dan198gg <danzelgoncharenko@gmail.com>
2024-11-21 18:01:41 +0300

    Delegate With by

--- src/delegate/VetClinika.kt
+++ src/delegate/VetClinika.kt
@@ -1,12 +1,6 @@
 package delegate
 
 class VetClinika(val catDoctor: CatAibolit,
-                 val dogDoctor: DogAibolit): DogVet, CatVet {
-    override fun treat(d: Dog) {
-        dogDoctor.treat(d)
-    }
-
-    override fun treat(c: Cat) {
-        catDoctor.treat(c)
-    }
-}
+                 val dogDoctor: DogAibolit):
+    DogVet by dogDoctor,
+    CatVet by catDoctor
